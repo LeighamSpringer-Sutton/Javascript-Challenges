f4398d6e3274390d7a3f3aabeb545fc8626ddb47
Leigham Springer-Sutton <LeighamSS@hotmail.com>
2018-12-21 21:27:28 -0500

    Alphabetposition Challenge
    Given a string, replace every letter with its position in the alphabet.

---
+++ alphabetposition.js
@@ -0,0 +1,15 @@
+function alphabetPosition(text) {
+  text = text.replace(/\s+/g,'');
+  text = text.toLowerCase();
+  var alpha = {};
+  var answer = "";
+  var a = 97;
+  for (var i = 0; i<26; i++)
+    alpha[String.fromCharCode(a + i)] = i+1;
+  for (var i = 0; i<text.length; i++){
+    if (alpha[text[i]]!= undefined){
+      answer += alpha[text[i]]+ " ";
+    }
+  }
+  return answer.trim();
+}
